a7d0cf5c56051b693d5fb4d897601729f35e2d73
Michael <michaelhofmann987@gmail.com>
2019-10-07 14:35:29 -0700

    Updates

---
+++ precache-manifest.8f08e7cfba01977797b4eea16da6f684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61fb0acbdcd71098b34b69dd1ffb235",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "f83d201173fdc05b72d9",
+    "url": "/project-4-assessment/static/css/main.c04c135c.chunk.css"
+  },
+  {
+    "revision": "19d4402d8d9427a7201c",
+    "url": "/project-4-assessment/static/js/2.6446d4ee.chunk.js"
+  },
+  {
+    "revision": "f83d201173fdc05b72d9",
+    "url": "/project-4-assessment/static/js/main.7976d459.chunk.js"
+  },
+  {
+    "revision": "75de8ec33a8a78802050",
+    "url": "/project-4-assessment/static/js/runtime-main.71652b97.js"
+  }
+]);
